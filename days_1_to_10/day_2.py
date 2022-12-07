bab9a819a0b8a94e6a6d74f641d7dda74e80b578
christian-the-brewer <brewer.christiang@gmail.com>
2022-12-07 13:12:35 -0500

    Completes day 2

--- days_1_to_10/day_2.py
+++ days_1_to_10/day_2.py
@@ -29,8 +29,35 @@
 # second_digit = int(two_digit_number[1])
 # print(f"{first_digit} + {second_digit} = {first_digit + second_digit}")
 
-# Coding exercise BMI calculator
-height = float(input("Enter your height in meters: "))
-weight = float(input("Enter your weight in kilograms: "))
-# bmi = str(weight / (height ** 2))
-print(f"Your BMI is {str(int(weight / (height ** 2)))}")
+# # Coding exercise BMI calculator
+# height = float(input("Enter your height in meters: "))
+# weight = float(input("Enter your weight in kilograms: "))
+# # bmi = str(weight / (height ** 2))
+# print(f"Your BMI is {str(int(weight / (height ** 2)))}")
+
+# #rounding
+# print(round(8/3, 2)) #rounds 2.66666 down to 2 decimal places
+
+# print(8 // 3) #floor division turns result into int
+
+#coding exercise - Your life in weeks
+# calculate how many days, weeks, and months left until you reach 90
+
+# #1 year = 365 days = 52 weeks = 12 months
+# while True:
+#     age = float(input("What is your age?: "))
+#     if 0 < age < 90:
+#         break
+#     else:
+#         print("Please enter an age between 1 and 90 years old")
+# years_left = 90 - age
+# print(f"You have {years_left * 365} days, {years_left * 52} weeks, and {years_left * 12} months left.")
+
+#Code exercise Tip Calculator
+print("Welcome to the tip calculator!")
+total = float(input("What was the bill total?: "))
+percentage = (float(input("What percentage would you like to tip?: ")) / 100 + 1)
+people = int(input("how many people will split the bill?: "))
+share = (total * percentage) / people
+print(f"Each person should pay: {share}")
+
