02744f5de95320daa511a00e8e80a275a4ac5c7a
christian-the-brewer <brewer.christiang@gmail.com>
2022-12-07 19:53:09 -0500

    Pizza Order exercise

--- days_1_to_10/day_3.py
+++ days_1_to_10/day_3.py
@@ -13,32 +13,80 @@
 # else:
 #     print("Not leap year.")
 
-#Ticket exercise
-ticket = 0
+# #Ticket exercise
+# ticket = 0
 
-print("Welcome to the rollercoaster!")
+# print("Welcome to the rollercoaster!")
 
-height = int(input("What is your height in cm?: "))
-if height >= 120:
-    age = int(input("How old are you?: "))
-    if age < 12:
-        print("Child tickets are $5.")
-        ticket += 5
-    elif age < 18:
-        print("Youth tickets are $7.")
-        ticket += 7
+# height = int(input("What is your height in cm?: "))
+# if height >= 120:
+#     age = int(input("How old are you?: "))
+#     if age < 12:
+#         print("Child tickets are $5.")
+#         ticket += 5
+#     elif age < 18:
+#         print("Youth tickets are $7.")
+#         ticket += 7
+#     else:
+#         print("Adult tickets are $12.")
+#         ticket += 12
+#     while True:
+#         photo_choice = input("Would you like a photo for only $3 more? y/n: ")
+#         if photo_choice == "y":
+#             ticket += 3
+#             break
+#         elif photo_choice == "n":
+#             break
+#         else:
+#             print("Please enter y or n: ")
+#     print(f"Your total comes to ${ticket}.")
+# else:
+#     print("Sorry, but the minimum height to ride is 120cm.")
+
+#Coding Exercise Pizza Ordering
+#small pizza = 15 medium = 20 large = 25
+#pepperoni for s = 2  m/l = 3
+#extra cheese = 1
+
+total = 0
+pepperoni = 0
+cheese = 1
+print("Python Pizza")
+
+while True:
+    size = input("Small Pizza: $15\nMedium Pizza: $20\nLarge Pizza: $25\nWhich size? S, M, or L?: ")
+    if size == "S" or size == "s":
+        pepperoni = 2
+        total += 15
+        break
+    elif size == "M" or size == "m":
+        pepperoni = 3
+        total += 20
+        break
+    elif size == "L" or size == "l":
+        pepperoni = 3
+        total += 25
+        break
     else:
-        print("Adult tickets are $12.")
-        ticket += 12
-    while True:
-        photo_choice = input("Would you like a photo for only $3 more? y/n: ")
-        if photo_choice == "y":
-            ticket += 3
-            break
-        elif photo_choice == "n":
-            break
-        else:
-            print("Please enter y or n: ")
-    print(f"Your total comes to ${ticket}.")
-else:
-    print("Sorry, but the minimum height to ride is 120cm.")
+        print("Please enter S, M, or L: ")
+
+while True:
+    add_pepperoni = input(f"Would you like to add pepperoni for ${pepperoni}? Y/N: ")
+    if add_pepperoni == "Y" or  add_pepperoni == "y":
+        total += pepperoni
+        break
+    elif add_pepperoni == "N" or add_pepperoni == "n":
+        break
+    else:
+        print("Please enter Y or N: ")
+
+while True:
+    extra_cheese = input(f"Would you like to add extra cheese for ${cheese}? Y/N: ")
+    if extra_cheese == "Y" or extra_cheese == "y":
+        total += cheese
+        break
+    elif extra_cheese == "N" or extra_cheese == "n":
+        break
+    else:
+        print("Please enter Y or N: ")
+print(f"Your final bill is ${total}")
