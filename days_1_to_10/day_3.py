ae0f81a34bfc677b9317edd15a9727a896e4a746
christian-the-brewer <brewer.christiang@gmail.com>
2022-12-07 20:45:24 -0500

    Completes love calculator exercise

--- days_1_to_10/day_3.py
+++ days_1_to_10/day_3.py
@@ -27,6 +27,8 @@
 #     elif age < 18:
 #         print("Youth tickets are $7.")
 #         ticket += 7
+#     elif 45 <= age <= 55:
+#         print("Wow your life is almost over, ride for free!")
 #     else:
 #         print("Adult tickets are $12.")
 #         ticket += 12
@@ -43,50 +45,68 @@
 # else:
 #     print("Sorry, but the minimum height to ride is 120cm.")
 
-#Coding Exercise Pizza Ordering
-#small pizza = 15 medium = 20 large = 25
-#pepperoni for s = 2  m/l = 3
-#extra cheese = 1
+# #Coding Exercise Pizza Ordering
+# #small pizza = 15 medium = 20 large = 25
+# #pepperoni for s = 2  m/l = 3
+# #extra cheese = 1
 
-total = 0
-pepperoni = 0
-cheese = 1
-print("Python Pizza")
+# total = 0
+# pepperoni = 0
+# cheese = 1
+# print("Python Pizza")
 
-while True:
-    size = input("Small Pizza: $15\nMedium Pizza: $20\nLarge Pizza: $25\nWhich size? S, M, or L?: ")
-    if size == "S" or size == "s":
-        pepperoni = 2
-        total += 15
-        break
-    elif size == "M" or size == "m":
-        pepperoni = 3
-        total += 20
-        break
-    elif size == "L" or size == "l":
-        pepperoni = 3
-        total += 25
-        break
-    else:
-        print("Please enter S, M, or L: ")
+# while True:
+#     size = input("Small Pizza: $15\nMedium Pizza: $20\nLarge Pizza: $25\nWhich size? S, M, or L?: ")
+#     if size == "S" or size == "s":
+#         pepperoni = 2
+#         total += 15
+#         break
+#     elif size == "M" or size == "m":
+#         pepperoni = 3
+#         total += 20
+#         break
+#     elif size == "L" or size == "l":
+#         pepperoni = 3
+#         total += 25
+#         break
+#     else:
+#         print("Please enter S, M, or L: ")
 
-while True:
-    add_pepperoni = input(f"Would you like to add pepperoni for ${pepperoni}? Y/N: ")
-    if add_pepperoni == "Y" or  add_pepperoni == "y":
-        total += pepperoni
-        break
-    elif add_pepperoni == "N" or add_pepperoni == "n":
-        break
-    else:
-        print("Please enter Y or N: ")
+# while True:
+#     add_pepperoni = input(f"Would you like to add pepperoni for ${pepperoni}? Y/N: ")
+#     if add_pepperoni == "Y" or  add_pepperoni == "y":
+#         total += pepperoni
+#         break
+#     elif add_pepperoni == "N" or add_pepperoni == "n":
+#         break
+#     else:
+#         print("Please enter Y or N: ")
 
-while True:
-    extra_cheese = input(f"Would you like to add extra cheese for ${cheese}? Y/N: ")
-    if extra_cheese == "Y" or extra_cheese == "y":
-        total += cheese
-        break
-    elif extra_cheese == "N" or extra_cheese == "n":
-        break
-    else:
-        print("Please enter Y or N: ")
-print(f"Your final bill is ${total}")
+# while True:
+#     extra_cheese = input(f"Would you like to add extra cheese for ${cheese}? Y/N: ")
+#     if extra_cheese == "Y" or extra_cheese == "y":
+#         total += cheese
+#         break
+#     elif extra_cheese == "N" or extra_cheese == "n":
+#         break
+#     else:
+#         print("Please enter Y or N: ")
+# print(f"Your final bill is ${total}")
+
+
+#Code exercise Love Calculator
+
+first = input("Love Calculator\nEnter your name: ").lower()
+second = input("Enter your partner's name: ").lower()
+name = first + second
+true_count = name.count("t") + name.count("r") + name.count("u") + name.count("e")
+love_count = name.count("l") + name.count("o") + name.count("v") + name.count("e")
+score = str(true_count) + str(love_count)
+if score == "00":
+    print("Your score is 0, you go together like coke and mentos.")
+elif int(score) < 10 or int(score) > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif 40 <= int(score) <= 50:
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}")
