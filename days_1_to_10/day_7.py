632ab4148214b0e041d1f534fbea19108d083af1
christian-the-brewer <brewer.christiang@gmail.com>
2022-12-10 22:41:40 -0500

    Adds hangman functionality to chose random word, take user guess, and determine if lettter is in word

--- days_1_to_10/day_7.py
+++ days_1_to_10/day_7.py
@@ -17,6 +17,7 @@ word_list = ["aardvark", "baboon", "camel"]
 correct_guesses = []
 lives = 5
 empty_board = ""
+game_won = False
 
 #randomly choose word
 chosen_word = word_list[random.randint(0,len(word_list)-1)]
@@ -28,17 +29,24 @@ for char in chosen_word:
 
 #check if letter guessed is in the chosen_word and add to correct_guess if it is
 def check_letter(letter, word):
+    global lives
     if letter in word:
         correct_guesses.append(letter)
+    else:
+        lives -= 1
 
 #function to print the gameboard 
 def print_blanks():
+    global game_won
     displayed_word = ""
     for char in chosen_word:
         if char in correct_guesses:
             displayed_word += char
         else:
             displayed_word += "_"
+    #determine win state by checking if the displayed_word contains any underscores
+    if "_" not in displayed_word:
+        game_won = True 
     return displayed_word
 
 
@@ -51,5 +59,17 @@ while lives > 0:
     guess = input("Guess a letter!\n").lower()
 
     check_letter(guess, chosen_word)
+    if chosen_word.split() in correct_guesses:
+        game_won = True
+    #If lives reach 0 and no win condition has been found end game with you lsot and display word
+    if lives == 0:
+        print(f"You lose! The word was {chosen_word}!")
+        break
+    elif game_won == True:
+        print(f"You win! The word was {chosen_word}.")
+        break
+   
+    
 
-    print_blanks()
+    print(print_blanks())
+
