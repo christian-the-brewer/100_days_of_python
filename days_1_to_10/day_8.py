3d91c466df256a810f24ccab114e664eb550eaaf
christian-the-brewer <brewer.christiang@gmail.com>
2022-12-12 17:00:09 -0500

    Starts day 8

---
+++ days_1_to_10/day_8.py
@@ -0,0 +1,24 @@
+#Day 8 
+
+# #functions continued
+# def greet():
+#     print("Hello")
+#     print("Hi")
+#     print("sup")
+
+# def greet_with(name, location):
+#     print(f"Hello {name}, from {location}")
+
+# greet_with("Christian", "Malden")
+
+# greet_with(location="Malden", name="Christian")
+
+# #Coding Challenge Paint can Calculator
+
+# import math 
+
+# def paint_can_calculate(width, height, can):
+#     total_area = float(width) * float(height)
+#     return math.ceil(total_area / float(can)) 
+
+# print(paint_can_calculate(5.4,6.1,5))
