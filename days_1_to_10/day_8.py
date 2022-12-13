f5b3526f6ba37f4de8b432d6570cd89cfbb8fd5a
christian-the-brewer <brewer.christiang@gmail.com>
2022-12-13 15:29:52 -0500

    Starts cesaer cypher program

--- days_1_to_10/day_8.py
+++ days_1_to_10/day_8.py
@@ -25,19 +25,42 @@
 
 #Prime number checker
 
-#check modulo of every number up to half of the selected number
+# #check modulo of every number up to half of the selected number
 
-def check_prime(number):
-    if number == 1:
-        return False
-    i = 2
-    while i <= number / 2:
-        if number % i == 0:
-            print(i)
-            return False
-        else:
-            print(i)
-            i += 1
-    return True
+# def check_prime(number):
+#     if number == 1:
+#         return False
+#     i = 2
+#     while i <= number / 2:
+#         if number % i == 0:
+#             print(i)
+#             return False
+#         else:
+#             print(i)
+#             i += 1
+#     return True
 
-print(check_prime(1))
+# print(check_prime(1))
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+while True:
+    direction = input("Enter 'encode' to encrypt a message, or decode to decrypt it:\n").lower()
+    if direction == "encode":
+        message = input("Enter your message to be   encrypted:\n").lower()
+        break
+    elif direction == "decode":
+        code = input("Enter your code to be decrypted:\n"). lower()
+        break
+    else:
+        print("Please enter 'encode' or 'decode'")
+        
+while True:
+    shift = input("Enter the shift number you would like to use:\n")
+    if isinstance(shift, int) and shift > 0:
+        break
+    else:
+        print("Please enter a positive whole number")
+
+def encode(string, shift):
+    
