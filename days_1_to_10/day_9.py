9eecb5a76a73ed13fe1f5d0039b5d5e6b735100d
christian-the-brewer <brewer.christiang@gmail.com>
2022-12-19 17:10:21 -0500

    Starts day 9

---
+++ days_1_to_10/day_9.py
@@ -0,0 +1,49 @@
+#Day 9
+# #Coding Challenge
+
+# student_scores = {
+#   "Harry": 81,
+#   "Ron": 78,
+#   "Hermione": 99, 
+#   "Draco": 74,
+#   "Neville": 62,
+# }
+
+# #Empty dictionary called student_grades
+# student_grades = {}
+
+# for student in student_scores:
+    
+#     grade = ""
+#     if student_scores[student] > 90:
+#         grade = "Oustanding"
+#     elif student_scores[student] > 80:
+#         grade = "Exceeds Expectations"
+#     elif student_scores[student] > 70:
+#         grade = "Acceptable"
+#     else:
+#         grade = "Fail"
+#     student_grades[student] = grade
+
+# print(student_grades)
+
+#Nesting
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+#Nesting list inside dictionary
+travel_log = {
+    "France": [
+        "Paris",
+        "Lille",
+        "Dijon"
+    ],
+    "Germany": [
+        "Berlin",
+        "Hamburg",
+        "Stuttgart"
+    ],
+}
