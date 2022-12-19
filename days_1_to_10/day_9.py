01160ca22820c7037c3f27ace6224843c59e8966
christian-the-brewer <brewer.christiang@gmail.com>
2022-12-19 17:51:35 -0500

    Completes second challenge

--- days_1_to_10/day_9.py
+++ days_1_to_10/day_9.py
@@ -27,54 +27,79 @@
 
 # print(student_grades)
 
-#Nesting
+# #Nesting
 
-capitals = {
-    "France": "Paris",
-    "Germany": "Berlin",
-}
+# capitals = {
+#     "France": "Paris",
+#     "Germany": "Berlin",
+# }
 
-#Nesting list inside dictionary
-travel_log = {
-    "France": [
-        "Paris",
-        "Lille",
-        "Dijon"
-    ],
-    "Germany": [
-        "Berlin",
-        "Hamburg",
-        "Stuttgart"
-    ],
-}
+# #Nesting list inside dictionary
+# travel_log = {
+#     "France": [
+#         "Paris",
+#         "Lille",
+#         "Dijon"
+#     ],
+#     "Germany": [
+#         "Berlin",
+#         "Hamburg",
+#         "Stuttgart"
+#     ],
+# }
 
-#Nesting list inside dictionary inside dictionary inside dictionary
-travel_log = {
-    "France": {"cities_visited": [
-        "Paris",
-        "Lille",
-        "Dijon"
-        ],
-        "total_visits": 12,
-    },
-    "Germany": {"cities_visited": [
-        "Berlin",
-        "Hamburg",
-        "Stuttgart"
-    ]},
-}
+# #Nesting list inside dictionary inside dictionary inside dictionary
+# travel_log = {
+#     "France": {"cities_visited": [
+#         "Paris",
+#         "Lille",
+#         "Dijon"
+#         ],
+#         "total_visits": 12,
+#     },
+#     "Germany": {"cities_visited": [
+#         "Berlin",
+#         "Hamburg",
+#         "Stuttgart"
+#     ]},
+# }
 
-#Nesting dicts in lists
+# #Nesting dicts in lists
 
+# travel_log = [
+#     {
+#         "country": "France",
+#         "cities_visited": ["Paris", "Lille", "Dijon"],
+#         "total_visits": 12,
+#     },
+#     {
+#         "country": "Germany",
+#         "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+#         "total_visits": 5,
+#     },
+# ]
+
+#Coding Challenge
 travel_log = [
-    {
-        "country": "France",
-        "cities_visited": ["Paris", "Lille", "Dijon"],
-        "total_visits": 12,
-    },
-    {
-        "country": "Germany",
-        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-        "total_visits": 5,
-    },
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
 ]
+
+#Function add_new_country
+def add_new_country(country_visited, times_visited, cities_visited):
+    new_country = {}
+    new_country["country"] = country_visited
+    new_country["visits"] = times_visited
+    new_country["cities"] = cities_visited
+    travel_log.append(new_country)
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
