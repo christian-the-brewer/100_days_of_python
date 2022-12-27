16b64ee86d544f2cd6160f59ca4bd916187cd39a
christian-the-brewer <brewer.christiang@gmail.com>
2022-12-27 11:28:05 -0500

    Starts day 9 silent bidding auction

--- days_1_to_10/day_9.py
+++ days_1_to_10/day_9.py
@@ -27,54 +27,81 @@
 
 # print(student_grades)
 
-#Nesting
+# #Nesting
 
-capitals = {
-    "France": "Paris",
-    "Germany": "Berlin",
-}
+# capitals = {
+#     "France": "Paris",
+#     "Germany": "Berlin",
+# }
 
-#Nesting list inside dictionary
-travel_log = {
-    "France": [
-        "Paris",
-        "Lille",
-        "Dijon"
-    ],
-    "Germany": [
-        "Berlin",
-        "Hamburg",
-        "Stuttgart"
-    ],
-}
+# #Nesting list inside dictionary
+# travel_log = {
+#     "France": [
+#         "Paris",
+#         "Lille",
+#         "Dijon"
+#     ],
+#     "Germany": [
+#         "Berlin",
+#         "Hamburg",
+#         "Stuttgart"
+#     ],
+# }
 
-#Nesting list inside dictionary inside dictionary inside dictionary
-travel_log = {
-    "France": {"cities_visited": [
-        "Paris",
-        "Lille",
-        "Dijon"
-        ],
-        "total_visits": 12,
-    },
-    "Germany": {"cities_visited": [
-        "Berlin",
-        "Hamburg",
-        "Stuttgart"
-    ]},
-}
+# #Nesting list inside dictionary inside dictionary inside dictionary
+# travel_log = {
+#     "France": {"cities_visited": [
+#         "Paris",
+#         "Lille",
+#         "Dijon"
+#         ],
+#         "total_visits": 12,
+#     },
+#     "Germany": {"cities_visited": [
+#         "Berlin",
+#         "Hamburg",
+#         "Stuttgart"
+#     ]},
+# }
 
-#Nesting dicts in lists
+# #Nesting dicts in lists
 
-travel_log = [
-    {
-        "country": "France",
-        "cities_visited": ["Paris", "Lille", "Dijon"],
-        "total_visits": 12,
-    },
-    {
-        "country": "Germany",
-        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-        "total_visits": 5,
-    },
-]
+# travel_log = [
+#     {
+#         "country": "France",
+#         "cities_visited": ["Paris", "Lille", "Dijon"],
+#         "total_visits": 12,
+#     },
+#     {
+#         "country": "Germany",
+#         "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+#         "total_visits": 5,
+#     },
+# ]
+
+#Silent Auction
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bid(bidding_record):
+    high_bid = 0
+    for bidder in bidding_record:
+        bid = bidding_record[bidder]
+        if bid > high_bid:
+            high_bid = bid
+            winner = bidder
+    return winner
+
+while not bidding_finished:
+    name = input("Pleae enter your name: ")
+    price = input("Please enter your bid: $")
+    bids[name] = price
+    while True:
+        another_bidder = input("Are there other bidders after you? y/n: ").lower()
+        if another_bidder == 'n':
+            bidding_finished = True
+        elif another_bidder == 'y':
+
+
+
