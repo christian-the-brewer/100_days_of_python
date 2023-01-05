22cdd169ea7355a787ce9efbba4131bfc0d20036
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-05 16:53:51 -0500

    Starts day 13

--- days_1_to_10/day_10.py
+++ days_1_to_10/day_10.py
@@ -1,13 +1,63 @@
 #Day 10
 
-def format_name(f_name, l_name):
-    """"An example of a function that titles a first and last name"""
-    if f_name == "" or l_name == "":
-        return print("invalid inputs")
+# def format_name(f_name, l_name):
+#     """"An example of a function that titles a first and last name"""
+#     if f_name == "" or l_name == "":
+#         return print("invalid inputs")
 
-    return f"{f_name.title()} {l_name.title()}"
+#     return f"{f_name.title()} {l_name.title()}"
 
-print(format_name( "", "brewer"))
+# print(format_name( "", "brewer"))
 
-format_name()
+# format_name()
+
+#Calculator Challenge
+#functions for calculating
+
+from art import logo
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+def calculator():
+    print(logo)
+    num1 = int(input("Enter first number: "))
+
+    for key in operations:
+        print(key)
+
+    should_continue = True
+
+    while should_continue:
+
+        operation_sign = input("Press operation: ")
+
+        num2 = float(input("Enter next number: "))
+
+        calculate = operations[operation_sign]
+        answer = calculate(num1, num2)
+        print(f"{num1} {operation_sign} {num2} = {answer}")
+
+        if input(f"Type 'y' y to continue with {answer}, or 'n' to start over: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
+
 
