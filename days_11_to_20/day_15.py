4f86e396ecee6d740fa5ea805a05a9bb4f1f03c2
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-06 09:51:35 -0500

    Creates dicts for drinks and supplies as well as check function

--- days_11_to_20/day_15.py
+++ days_11_to_20/day_15.py
@@ -2,3 +2,44 @@
 
 # Coffee machine simulator
 
+import os
+from coffee_art import logo
+
+#coffee machine supplies
+supplies = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0,
+}
+
+
+#drinks
+espresso = {
+    "water": 50,
+    "milk": 0,
+    "coffee": 18,
+    "price": 1.5,
+}
+latte = {
+    "water": 200,
+    "milk": 150,
+    "coffee": 24,
+    "price": 2.5,
+}
+cappuccino = {
+    "water": 250,
+    "milk": 100,
+    "coffee": 24,
+    "price": 3.0,
+}
+
+#check if enough supplies
+def check(drink, supplies):
+    for supply in supplies:
+        if supply != "money":
+            if drink[supply] > supplies[supply]:
+                return False
+    return True
+
+print(check(espresso, supplies))
