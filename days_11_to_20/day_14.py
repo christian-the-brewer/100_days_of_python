d544c8bdd55b232df795ef0ba8a68a8f1ac16e13
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-05 23:04:09 -0500

    Adds basic game loop and functions to pick celebs and compare followers

--- days_11_to_20/day_14.py
+++ days_11_to_20/day_14.py
@@ -20,17 +20,46 @@ a = 0
 
 #function to pick celeb
 def pick_celeb(previous):
+    """Takes previous celeb and returns a different one"""
     while True:
         celeb = random.randint(0,len(data) - 1)
         if celeb != previous:
             return celeb
 
+#function to compare followers
+def compare(a, b):
+    """Takes two celebs and returns the one with more followers"""
+    if data[a]["follower_count"] > data[b]["follower_count"]:
+        return "a"
+    else:
+        return "b"
+
 while keep_playing:
+    a = pick_celeb(-1)
     while win_streak:
-        #select 
+        #select second  celeb
+        b = pick_celeb(a)
+        #first celeb data
+        a_name = data[a]["name"]
+        a_followers = data[a]["follower_count"]
+        a_description = data[a]["description"].lower()
+        a_country = data[a]["country"]
+        #second celeb data
+        b_name = data[b]["name"]
+        b_followers = data[b]["follower_count"]
+        b_description = data[b]["description"].lower()
+        b_country = data[b]["country"]
+
         print(logo)
+        print(f"Compare A: {a_name}, a {a_description}, from {a_country}.")
+        print(vs)
+        print(f"Against B: {b_name}, a {b_description}, from {b_country}.")
+        #take user guess
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
+        #compare followers
 
-
+        break
+    break
 
 
