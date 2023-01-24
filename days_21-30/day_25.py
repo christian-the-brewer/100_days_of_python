b84231d5f315e1f0d6317e54018eb26e4a3113d5
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-24 11:05:11 -0500

    installing pandas

--- days_21-30/day_25.py
+++ days_21-30/day_25.py
@@ -1,4 +1,12 @@
 #Day 25
-with open("./weather_data.csv") as data_file:
-    data = data.readlines()
+# import csv
+# with open("./weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temps = [int(day[1]) for day in data if day[1] != "temp"]
+#     for temp in temps:
+#         print(temp)
     
+import pandas as pd
+
+data = pd.Series([1,2,3])
+print(data)
