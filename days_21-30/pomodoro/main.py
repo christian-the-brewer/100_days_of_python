c3042ee4775e7ddb1efd6256f696267c8f8faefb
Christian Brewer <106411334+christian-the-brewer@users.noreply.github.com>
2023-09-25 15:42:44 -0400

    Update main.py

--- days_21-30/pomodoro/main.py
+++ days_21-30/pomodoro/main.py
@@ -1,102 +1,65 @@
-# Pomodoro Timer App by Christian Brewer
+# Language Learning flash card app by Christian Brewer
 
-from tkinter import *
-import math
+import tkinter
+import pandas as pd
+import random
+import time
 
-# Constants
-PINK = "#e2979c"
-RED = "#D80032"
-GREEN = "#9bdeac"
-BG_COLOR = "#F9DEC9"
-FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
-reps = 0
-checkmark_count = 0
-CHECKMARK = "✔"
-timer = None
+# word data
+df = pd.read_csv("it_to_en_1000_word_list.csv")
 
-# Timer Reset
-def reset_timer():
-  global reps, checkmark_count, timer
-  window.after_cancel(timer)
-  label_1.config(text="Timer", fg=RED)
-  canvas.itemconfig(timer_text, text="00:00")
-  reps = 0
-  checkmark_count = 0
-  checkmark_update()
+
+# colors
+
+BG_COLOR = "#B1DDC6"
+Font = "#26577C"
+window = tkinter.Tk()
+window.minsize(width=600, height=600)
+window.config(padx=50, pady=50, bg=BG_COLOR)
+window.title("Language Flash Cards")
+
+language_1 = "Italian"
+language_2 = "English"
+
+global num
+
+# random_word func
+
+
+def random_word():
+  num = random.randint(0, len(df))
+  canvas.itemconfig(shown_word,text=df.loc[num][language_1])
+  time.sleep(3)
   
-# Timer Mechanism
-def start_timer():
-  global reps, checkmark_count 
-  print(reps)
   
-  if reps == 7:
-    print("long break")
-    checkmark_update()
-    label_1.config(text="Long Break", fg="#F78CA2")
-    count_down(LONG_BREAK_MIN * 60)
-    reps = 0
-    checkmark_count = 0
-    checkmark_update()
-  elif reps % 2 == 0:
-    label_1.config(text="Focus Time", fg="#D80032")
-    print("work")
-    count_down(WORK_MIN * 60)
-    reps += 1
-    checkmark_count += 1
-  else:
-    label_1.config(text="Break Time", fg="#F78CA2")
-    checkmark_update()
-    print("short break")
-    count_down(SHORT_BREAK_MIN * 60)
-    reps += 1
 
-# Countdown
-def checkmark_update():
-  check_mark.config(text=f"{CHECKMARK * checkmark_count}")
 
-def count_down(count):
+word = "test"
 
-  count_min = math.floor(count / 60)
-  count_sec = count % 60
-  if count_min < 10:
-    count_min = f"0{count_min}"
-  if count_sec < 10:
-    count_sec = f"0{count_sec}"
-  canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
-  if count > 0:
-    global timer
-    timer = window.after(1000, count_down, count - 1)
-  else:
-    start_timer()
-    
+canvas = tkinter.Canvas(width=800, height=526,
+                        highlightthickness=0, bg=BG_COLOR)
 
-# UI
-window = Tk()
-window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=BG_COLOR)
+wrong_image = tkinter.PhotoImage(file="images/wrong.png")
+right_image = tkinter.PhotoImage(file="images/right.png")
+back_image = tkinter.PhotoImage(file="images/card_back.png")
+front_image = tkinter.PhotoImage(file="images/card_front.png")
 
-canvas = Canvas(width=200, height=224, bg=BG_COLOR, highlightthickness=0)
-tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text="00:00", fill="white",
-                   font=(FONT_NAME, 35, "bold"))
-canvas.grid(row=2, column=2)
+canvas.create_image(400, 263, image=front_image)
+canvas.grid(row=0, column=0, columnspan=2)
 
-#Timer Label
-label_1 = Label(text="Timer", bg=BG_COLOR, fg=RED, font=(FONT_NAME,35,"bold"))
-label_1.grid(row=1,column=2)
-#Start button
-start_button = Button(text="Start", command=start_timer)
-start_button.grid(row=3,column=1)
-#Reset button
-reset_button = Button(text="Reset", command=reset_timer)
-reset_button.grid(row=3,column=3)
-#checkmark
-check_mark = Label(bg=BG_COLOR, fg=GREEN)
-check_mark.grid(row=4,column=2)
+# text on canvas
+language = canvas.create_text(
+    400, 150, text=language_1, font=("Ariel", 40, "italic"),)
+shown_word = canvas.create_text(
+    400, 263, text=word, font=("Ariel", 60, "bold"), )
+
+# buttons
+wrong_button = tkinter.Button(
+    image=wrong_image, highlightthickness=0, command=random_word)
+wrong_button.grid(row=1, column=1)
+right_button = tkinter.Button(
+    image=right_image, highlightthickness=0, command=random_word)
+right_button.grid(row=1, column=0)
 
 
 window.mainloop()
